af46bd3a79b2b92f7c221891bd3a41dd5dd0cb8f
DELL <DELL@DESKTOP-PP11UR7.mshome.net>
2024-12-27 15:31:50 +0530

    my first commit

---
+++ Calculator/src/Calci/Calculator1.java
@@ -0,0 +1,12 @@
+package Calci;
+
+public class Calculator1 {
+	public void add()
+	{
+		int a=10;
+		int b=45;
+		int c=a+b;
+		System.out.println("a+b = "+c);
+			
+	}
+}
---
+++ Calculator/src/Calci/Test.java
@@ -0,0 +1,8 @@
+package Calci;
+
+public class Test {
+public static void main(String[] args) {
+	Calculator1 cl=new Calculator1();
+	cl.add();
+}
+}
